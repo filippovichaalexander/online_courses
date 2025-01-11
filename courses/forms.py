65bf0744819bbb8c6db265daf0cecdbecedc7bb7
filippovichaalexander <filippovichaalexander@gmail.com>
2025-01-11 17:49:18 +0300

    course details page, course parts page

--- courses/forms.py
+++ courses/forms.py
@@ -1,6 +1,21 @@
 from django import forms
-from .models import TopicDocument, CourseTopic
+from .models import TopicDocument, Course, CoursePart, CourseTopic
 
+class CourseForm(forms.ModelForm):
+    title = forms.CharField(max_length=255, required=True)
+    # part = forms.ModelChoiceField(
+    #     queryset=CoursePart.objects.all(),
+    #     empty_label="Select a course part",
+    #     required=True
+    # )
+    # topic = forms.ModelChoiceField(
+    #     queryset=CourseTopic.objects.all(),
+    #     empty_label="Select a part topic",
+    #     required=True
+    # )
+    class Meta:
+        model = Course
+        fields = ['title']
 
 class TopicDocumentForm(forms.ModelForm):
     topic = forms.ModelChoiceField(
@@ -12,3 +27,4 @@ class TopicDocumentForm(forms.ModelForm):
     class Meta:
         model = TopicDocument
         fields = ['name', 'file', 'topic']
+
--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,137 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-from .forms import TopicDocumentForm
-from .models import TopicDocument
+from .forms import TopicDocumentForm, CourseForm
+from .models import Course, CoursePart, CourseTopic, TopicDocument
 
+
+def courses_list(request):
+    courses = Course.objects.all()  # Fetching TopicDocument instances
+    parts = CoursePart.objects.all()
+    topics = CourseTopic.objects.all()
+
+    if request.method == 'POST':
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')  # Make sure to have a success URL
+    else:
+        form = CourseForm()  # Initialize the form for GET request
+
+    context = {
+        'form': form,  # Include the form in the context
+        'courses': courses,
+        # 'parts': parts,
+        # 'topics': topics
+    }
+    return render(request, 'courses_list.html', context)
+
+def course_details(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    parts = course.parts.all()
+    topics = CourseTopic.objects.filter(part__course=course)
+
+    context = {
+        'course': course,
+        'parts': parts,
+        'topics': topics,
+    }
+    return render(request, 'course_details.html', context)
+
+def part_details(request, course_id, part_id):
+    part = CoursePart.objects.get(id=part_id)
+    course = Course.objects.get(id=course_id)
+    topics = CourseTopic.objects.all()
+
+    context = {
+        'part': part,
+        'course': course,
+        'topics': topics,
+    }
+    return render(request, 'part_details.html', context)
+
+def update_course(request, course_id):
+    course = get_object_or_404(CourseForm, id=course_id)
+
+    if request.method == 'POST':
+        form = CourseForm(request.POST, instance=course)
+        if form.is_valid():
+            form.save()
+            return redirect('course_list')
+    else:
+        form = CourseForm(instance=course)
+
+    return render(request, 'update_course.html', {'form': form})
+
+
+def delete_course(request, course_id):
+    course = get_object_or_404(CourseForm, id=course_id)
+
+    if request.method == 'POST':
+        # delete file itself
+        # if document.file:
+        #     document.file.delete(save=False)
+        # delete db row
+        course.delete()
+        return redirect('courses_list')
+
+
+def update_part(request, part_id):
+    part = get_object_or_404(CoursePart, id=part_id)
+
+    if request.method == 'POST':
+        form = CoursePart(request.POST, instance=part)
+        if form.is_valid():
+            form.save()
+            return redirect('course_list')
+    else:
+        form = CoursePart(instance=part)
+
+    return render(request, 'update_part.html', {'form': form})
+
+
+def delete_part(request, part_id):
+    part = get_object_or_404(CourseTopic, id=part_id)
+
+    if request.method == 'POST':
+        # delete file itself
+        # if document.file:
+        #     document.file.delete(save=False)
+        # delete db row
+        part.delete()
+        return redirect('courses_list')
+
+    return render(request, 'confirm_delete.html', {'part': part})
+
+
+def update_topic(request, topic_id):
+    topic = get_object_or_404(CourseTopic, id=topic_id)
+
+    if request.method == 'POST':
+        form = CourseTopic(request.POST, instance=topic)
+        if form.is_valid():
+            form.save()
+            return redirect('course_list')
+    else:
+        form = CourseTopic(instance=topic)
+
+    return render(request, 'update_topic.html', {'form': form})
+
+
+def delete_topic(request, topic_id):
+    topic = get_object_or_404(CourseTopic, id=topic_id)
+
+    if request.method == 'POST':
+        # delete file itself
+        # if document.file:
+        #     document.file.delete(save=False)
+        # delete db row
+        topic.delete()
+        return redirect('courses_list')
+
+    return render(request, 'confirm_delete.html', {'topic': topic})
+
+
+# documents
 def document_list(request):
     if request.method == 'POST':
         form = TopicDocumentForm(request.POST, request.FILES)
@@ -14,6 +143,7 @@ def document_list(request):
 
     documents = TopicDocument.objects.all()
     return render(request, 'document_list.html', {'form': form, 'documents': documents})
+
 
 def update_document(request, document_id):
     document = get_object_or_404(TopicDocument, id=document_id)
@@ -27,6 +157,7 @@ def update_document(request, document_id):
         form = TopicDocumentForm(instance=document)
 
     return render(request, 'update_document.html', {'form': form, 'document': document})
+
 
 def delete_document(request, document_id):
     document = get_object_or_404(TopicDocument, id=document_id)
